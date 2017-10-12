956004ab62756c77f9d5e869608cd0e5247a9960
mohican16 <faceitall@yandex.ru>
2017-10-12 09:48:40 +0300

    Bullhead: Convert to GlassCannon

--- init.bullhead.power.sh
+++ init.bullhead.power.sh
@@ -37,7 +37,7 @@ get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
 restorecon -R /sys/devices/system/cpu
 
 # Best effort limiting for first time boot if msm_performance module is absent
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 1248000
 
 # Limit A57 max freq from msm_perf module in case CPU 4 is offline
 write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000"
@@ -57,17 +57,21 @@ write /sys/module/lpm_levels/system/a57/a57-l2-retention/idle_enabled 0
 # configure governor settings for little cluster
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460800:75 960000:80"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1440000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 93
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay "0 600000:19000 787200:20000 960000:24000 1248000:38000"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 50000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 600000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_slack 380000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "29 384000:88 600000:90 787200:92 960000:93 1248000:98"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 60000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/boost 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/align_windows 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/boostpulse_duration 0
 
 # online CPU4
 write /sys/devices/system/cpu/cpu4/online 1
@@ -75,17 +79,21 @@ write /sys/devices/system/cpu/cpu4/online 1
 # configure governor settings for big cluster
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 1824000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 150
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay "20000 960000:60000 1248000:30000"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 60000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 633600
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_slack 380000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads 98
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 60000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/boost 0
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/align_windows 1
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 20000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 633600
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 0
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 0
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/boostpulse_duration 0
 
 # restore A57's max
 copy /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
@@ -97,8 +105,16 @@ write /sys/devices/system/cpu/cpu5/online 1
 write /sys/module/msm_performance/parameters/cpu_max_freq "4:4294967295 5:4294967295"
 
 # input boost configuration
-write /sys/module/cpu_boost/parameters/input_boost_freq "0:960000"
-write /sys/module/cpu_boost/parameters/input_boost_ms 40
+write /sys/module/cpu_boost/parameters/input_boost_enabled 1
+write /sys/module/cpu_boost/parameters/boost_ms 40
+write /sys/module/cpu_boost/parameters/input_boost_freq "0:600000 1:600000 2:600000 3:600000 4:633600 5:633600"
+write /sys/module/cpu_boost/parameters/input_boost_ms 300
+write /sys/module/cpu_boost/parameters/load_based_syncs Y
+write /sys/module/cpu_boost/parameters/migration_load_threshold 15
+write /sys/module/cpu_boost/parameters/sync_threshold 1248000
+
+# Backlight dimmer
+write /sys/module/mdss_fb/parameters/backlight_dimmer 1
 
 # Setting B.L scheduler parameters
 write /proc/sys/kernel/sched_migration_fixup 1
